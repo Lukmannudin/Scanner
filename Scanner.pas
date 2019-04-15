3f2f866cbf02454d664be296bb9ca2412086cb00
Lukmannudin <laodelukmannudinpriatna@gmail.com>
2019-04-15 15:37:50 +0700

    add count category

--- Scanner.pas
+++ Scanner.pas
@@ -9,6 +9,7 @@ var
     sizeOfChar:integer;
     dataToken: array of Char;
     extraIncrement : integer;
+    intcon,realcon,charcon,stringt,notsy,plus,minus,times,idiv,rdiv,imod,andsy,orsy,egl,neg,gtr,geg,lss,leg,lparent,rparent,lbrack,rbrack,comma,semicolon,period,colon,becomes,constsy,typesy,varsy,functionsy,proceduresy,arraysy,recordsy,programsy,ident,beginsy,ifsy,casesy,repeatsy,whilesy,forsy,endsy,elsesy,untilsy,ofsy,dosy,tosy,downtosy,thensy,typeint,typechar,typereal : integer;
 
     function countChar():integer;
     var
@@ -44,6 +45,13 @@ var
 
 
 begin
+    intcon:= 0;realcon:=0;charcon:=0;stringt:=0;notsy:=0;plus:=0;minus:=0;times:=0;idiv:=0;rdiv:=0;imod:=0;
+    andsy:=0;orsy:=0;egl:=0;neg:=0;gtr:=0;geg:=0;lss:=0;leg:=0;lparent:=0;rparent:=0;lbrack:=0;comma:=0;
+    semicolon:=0;period:=0;colon:=0;becomes:=0;constsy:=0;typesy:=0;varsy:=0;functionsy:=0;proceduresy:=0;
+    arraysy:=0;recordsy:=0;programsy:=0;ident:=0;beginsy:=0;ifsy:=0;casesy:=0;repeatsy:=0;whilesy:=0;
+    forsy:=0;endsy:=0;elsesy:=0;untilsy:=0;ofsy:=0;dosy:=0;tosy:=0;downtosy:=0;thensy:=0;typeint:=0;
+    typechar:=0;typereal:=0;
+    
     SetLength(dataToken,countChar());
     charLoad(dataToken);
     i:=0;
@@ -59,6 +67,7 @@ begin
             (UpperCase(dataToken[i+5])='E') and  
             (UpperCase(dataToken[i+6])='R') then
             begin
+                intcon+=1;
                 writeln('tipe data integer');
             end
         else
@@ -67,14 +76,7 @@ begin
             (UpperCase(dataToken[i+2])='A') and 
             (UpperCase(dataToken[i+3])='L') then
             begin
-                writeln('tipe data real');
-            end
-        else
-        if (UpperCase(dataToken[i+0])='R') and 
-            (UpperCase(dataToken[i+1])='E') and 
-            (UpperCase(dataToken[i+2])='A') and 
-            (UpperCase(dataToken[i+3])='L') then
-            begin
+                realcon+=1;
                 writeln('tipe data real');
             end
         else
@@ -83,6 +85,7 @@ begin
             (UpperCase(dataToken[i+2])='A') and 
             (UpperCase(dataToken[i+3])='R') then
             begin
+                charcon += 1;
                 writeln('tipe data char');
             end
         else
@@ -93,6 +96,7 @@ begin
             (UpperCase(dataToken[i+4])='N') and
             (UpperCase(dataToken[i+5])='G') then
             begin
+                stringt +=1;
                 writeln('tipe data string');
             end
         else
@@ -100,33 +104,39 @@ begin
             (UpperCase(dataToken[i+1])='O') and 
             (UpperCase(dataToken[i+2])='T') then
             begin
+                notsy += 1;
                 writeln('operator not');
             end
         else
         if  (UpperCase(dataToken[i+0])='+') then
             begin
-              writeln('operator +');
+                plus += 1;
+                writeln('operator +');
             end
         else
         if  (UpperCase(dataToken[i+0])='-') then
             begin
-              writeln('operator -');
+                minus += 1;
+                writeln('operator -');
             end
         else    
         if  (UpperCase(dataToken[i+0])='*') then
             begin
-              writeln('operator *');
+                times += 1;
+                writeln('operator *');
             end
         else
         if  (UpperCase(dataToken[i+0])='D') and 
             (UpperCase(dataToken[i+1])='I') and 
             (UpperCase(dataToken[i+2])='V') then
             begin
+                idiv += 1;
                 writeln('operator div');
             end
         else
         if  (UpperCase(dataToken[i+0])='/') then 
             begin
+                rdiv += 1;
                 writeln('operator /');
             end
         else
@@ -134,6 +144,7 @@ begin
             (UpperCase(dataToken[i+1])='O') and 
             (UpperCase(dataToken[i+2])='D') then
             begin
+                imod += 1;
                 writeln('operator mod');
             end
         else
@@ -141,17 +152,20 @@ begin
             (UpperCase(dataToken[i+1])='N') and 
             (UpperCase(dataToken[i+2])='D') then
             begin
+                andsy += 1;
                 writeln('operator and');
             end
         else
         if  (UpperCase(dataToken[i+0])='O') and 
             (UpperCase(dataToken[i+1])='R') then
             begin
+                orsy += 1;
                 writeln('operator or');
             end
         else
         if  (UpperCase(dataToken[i+0])='=') then
             begin
+                egl+=1;
                 writeln('operator =');
             end
         else
@@ -160,20 +174,25 @@ begin
                 if (UpperCase(dataToken[i+1])='=') then
                  begin
                     writeln('operator <=');
+                    leg+=1;
                  end else if(UpperCase(dataToken[i+1])='>') then
                    begin
                      writeln('operator <>');
+                     neg +=1;
                    end else 
+                        lss+=1;
                         writeln('operator <');
             end
         else 
         if  (UpperCase(dataToken[i+0])='(') then
             begin
+                lparent += 1;
                 writeln('operator (');
             end
         else  
         if  (UpperCase(dataToken[i+0])=')') then
             begin
+                rparent += 1;
                 writeln('operator )');
             end
         else 
@@ -182,32 +201,39 @@ begin
                 if (UpperCase(dataToken[i+1])='=') then
                  begin
                     writeln('operator >=');
+                    geg+=1;
                  end else 
+                     gtr +=1;
                      writeln('operator >');
             end
         else 
         if  (UpperCase(dataToken[i+0])='[') then
             begin
+                lbrack += 1;
               writeln('simbol [');
             end
         else 
         if  (UpperCase(dataToken[i+0])=']') then
             begin
+                rbrack += 1;
               writeln('simbol ]');
             end
         else
         if  (UpperCase(dataToken[i+0])=',') then
             begin
+            comma +=1;
               writeln('simbol ,');
             end
         else
         if  (UpperCase(dataToken[i+0])=';') then
             begin
+                semicolon += 1;
               writeln('simbol ;');
             end
         else
         if  (UpperCase(dataToken[i+0])='.') then
             begin
+                period += 1;
               writeln('simbol .');
             end
         else
@@ -216,7 +242,9 @@ begin
               if (UpperCase(dataToken[i+1])='=') then
                 begin
                   writeln('operator :=');
+                  becomes +=1;
                 end else 
+                    colon += 1;
                     writeln('operator :');
             end
         else
@@ -226,6 +254,7 @@ begin
             (UpperCase(dataToken[i+3])='S') and 
             (UpperCase(dataToken[i+4])='T') then
             begin
+                constsy +=1;
                 writeln('keyword const');
             end
         else
@@ -233,6 +262,7 @@ begin
             (UpperCase(dataToken[i+1])='a') and 
             (UpperCase(dataToken[i+2])='r') then
             begin
+                varsy +=1;
                 writeln('keyword var');
             end
         else
@@ -245,6 +275,7 @@ begin
             (UpperCase(dataToken[i+5])='O') and  
             (UpperCase(dataToken[i+6])='N') then
             begin
+            functionsy +=1;
                 writeln('keyword function');
             end
         else 
@@ -258,6 +289,7 @@ begin
             (UpperCase(dataToken[i+6])='R') and  
             (UpperCase(dataToken[i+7])='E') then
             begin
+                proceduresy +=1;
                 writeln('keyword procedure');
             end
         else  
@@ -267,6 +299,7 @@ begin
             (UpperCase(dataToken[i+3])='A') and 
             (UpperCase(dataToken[i+4])='Y') then
             begin
+                arraysy +=1;
                 writeln('keyword array');
             end
         else  
@@ -277,6 +310,7 @@ begin
             (UpperCase(dataToken[i+4])='R') and
             (UpperCase(dataToken[i+5])='D') then
             begin
+                period +=1;
                 writeln('keyword record');
             end
         else  
@@ -288,6 +322,7 @@ begin
             (UpperCase(dataToken[i+5])='A') and 
             (UpperCase(dataToken[i+5])='M') then
             begin
+                programsy +=1;
                 writeln('keyword program');
             end
         else   
@@ -297,12 +332,14 @@ begin
             (UpperCase(dataToken[i+3])='I') and 
             (UpperCase(dataToken[i+4])='N') then
             begin
+                beginsy +=1;
                 writeln('keyword begin');
             end
         else    
         if  (UpperCase(dataToken[i+0])='I') and 
             (UpperCase(dataToken[i+1])='F') then
             begin
+                ifsy +=1;
                 writeln('keyword if');
             end
         else   
@@ -311,6 +348,7 @@ begin
             (UpperCase(dataToken[i+2])='S') and 
             (UpperCase(dataToken[i+3])='E') then
             begin
+                casesy +=1;
                 writeln('keyword case');
             end
         else  
@@ -321,6 +359,7 @@ begin
             (UpperCase(dataToken[i+4])='A') and
             (UpperCase(dataToken[i+5])='T') then
             begin
+                repeatsy +=1;
                 writeln('keyword repeat');
             end
         else    
@@ -330,6 +369,7 @@ begin
             (UpperCase(dataToken[i+3])='L') and 
             (UpperCase(dataToken[i+4])='E') then
             begin
+                whilesy +=1;
                 writeln('keyword while');
             end
         else   
@@ -338,6 +378,7 @@ begin
             (UpperCase(dataToken[i+1])='N') and 
             (UpperCase(dataToken[i+2])='D') then
             begin
+                endsy +=1;
                 writeln('keyword end');
             end
         else    
@@ -346,6 +387,7 @@ begin
             (UpperCase(dataToken[i+2])='S') and 
             (UpperCase(dataToken[i+3])='E') then
             begin
+                elsesy +=1;
                 writeln('keyword else');
             end
         else    
@@ -355,24 +397,28 @@ begin
             (UpperCase(dataToken[i+3])='I') and 
             (UpperCase(dataToken[i+4])='L') then
             begin
+                untilsy +=1;
                 writeln('keyword until');
             end
         else     
         if  (UpperCase(dataToken[i+0])='O') and 
             (UpperCase(dataToken[i+1])='F') then
             begin
+                ofsy +=1;
                 writeln('keyword of');
             end
         else     
         if  (UpperCase(dataToken[i+0])='D') and 
             (UpperCase(dataToken[i+1])='O') then
             begin
+                dosy +=1;
                 writeln('keyword do');
             end
         else    
         if  (UpperCase(dataToken[i+0])='T') and 
             (UpperCase(dataToken[i+1])='O') then
             begin
+                tosy +=1;
                 writeln('keyword to');
             end
         else  
@@ -383,6 +429,7 @@ begin
             (UpperCase(dataToken[i+4])='T') and
             (UpperCase(dataToken[i+5])='O') then
             begin
+                downtosy +=1;
                 writeln('keyword downto');
             end
         else   
@@ -391,6 +438,7 @@ begin
             (UpperCase(dataToken[i+2])='E') and 
             (UpperCase(dataToken[i+3])='N') then
             begin
+                thensy +=1;
                 writeln('keyword then');
             end
         else 
